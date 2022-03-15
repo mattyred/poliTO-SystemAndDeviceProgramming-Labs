adeb73fd327ffa4c3c73099591e22a6b11d18cb9
mattyred <mattiarosso99@gmail.com>
2022-03-15 14:04:08 +0100

    modified Exercise1.c

--- LabQ_02/Exercise1.c
+++ LabQ_02/Exercise1.c
@@ -17,41 +17,36 @@ struct person_t{
 };
 
 int main(int argc, char* argv[]){
-	int fd_file1, fd_file2, nR, nW, i=0;
+	int fd_binary_file, nR, nW, i=0;
 	char buf[BUFFSIZE];
-	FILE *fp_file1, *fp_file3;
+	FILE *fp_binary_file, *fp_out_text_file, *fp_file1;
 
-
-	/* Open file with fopen() */
-	fp_file1 = fopen(argv[1], "r");
+	fp_file1 = fopen(argv[1], "r"); // Open file1 as a text file -> fp_file1
 	
-	/* Read file with fscanf() and print on second file(binary)*/
-	fd_file2 = open (argv[2], 
-					 O_WRONLY | O_CREAT | O_TRUNC, 
-					 S_IRUSR | S_IWUSR);
+	fd_binary_file = open (argv[2], 
+							O_WRONLY | O_CREAT | O_TRUNC, 
+							S_IRUSR | S_IWUSR);// Open file2 as a binary file -> fp_binary_file
 	struct person_t p;
 	while (fscanf(fp_file1, "%d %ld %s %s %d", &p.ID, 
 									&p.register_number,
 									p.surname,
 									p.name,
-									&p.mark) != EOF){							
-		nW = write (fd_file2, &p, sizeof(p));								
-	}
-	close(fd_file2);
-	/* Read the written binary file */
-	fd_file1 = open (argv[2], O_RDONLY);
-	if(fd_file1 == -1){
-		fprintf(stdout,"Error opening file\n");
-		exit(1);
+									&p.mark) != EOF){
+		// printf("%d %ld %s %s %d\n",p.ID,p.register_number,p.surname,p.name,p.mark);							
+		nW = write (fd_binary_file, &p, sizeof(struct person_t));								
 	}
 	
-	fp_file3 = fopen(argv[3], "r");
+	close(fd_binary_file);
+	fd_binary_file = open (argv[2], O_RDONLY); // Open file2 as a binary file -> fd_binary_file
+
+	fp_out_text_file = fopen(argv[3], "r"); // Open file3 as a text file  -> fp_out_text_file
 	printf("Content of the binary file: \n");
-	while ( (nR = read (fd_file1, buf, BUFFSIZE)) > 0 ) {
-		
+
+	while ( (nR = read (fd_binary_file, buf, BUFFSIZE)) > 0 ) {
+		printf("%c",buf[i++]);
 	}
 	
-	close(fd_file1);
+
 	return 0;
 		
 } 
