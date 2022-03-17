943b32615b939c16984ed714c7050843bda05cb6
mattyred <mattiarosso99@gmail.com>
2022-03-17 18:50:41 +0100

    still in error in visiting . and ..

--- LabQ_02/Exercise3.c
+++ LabQ_02/Exercise3.c
@@ -10,40 +10,46 @@
 
 #define MAXC 31
 
-void explore_directory(char* full_name){
+void explore_object(char* full_name){
 	struct stat buf;
 	struct dirent *dirp;
+	char current_dir_name[MAXC];
 	DIR* dp;
-
-	// Get information about the current element
-	lstat(full_name, &buf); // segmentation fault here
 	
+	// Get information about the current element
+	lstat(full_name, &buf);
+
 	// Check if it is a file or a directory
 	if(S_ISREG(buf.st_mode) != 0){ // it is a file, return
-		printf(": It is a file\n");
+		printf("%s : It is a file\n",full_name);
 		return;
 	}else if(S_ISDIR(buf.st_mode) != 0){ // it is a directory, recur
-		printf(": It is a directory\n");
-		
-		// Check if it is not "." or ".." since i don't want to visit them
-		if((strcmp(full_name,".") != 0) && (strcmp(full_name, "..") != 0)){ // it is a normal directory
-			printf("normal directory");
-			// visit each element and if it is a directory visit it recursively
-			while((dirp = readdir(dp)) != NULL){
-				sprintf(full_name,"%s/%s",full_name,dirp->d_name);
-				printf("--Examinating the path: %s\n", full_name);
-		
-				explore_directory(full_name);
-			}	
-			return;
+		printf("%s : It is a directory\n", full_name);
+
+		// Open the directory
+		if((dp = opendir(full_name)) == NULL){
+			printf("Error in opening %s\n", full_name);
+			exit(1);
 		}
+			
+		// visit each element and if it is a directory visit it recursively
+		while((dirp = readdir(dp)) != NULL){
+			sprintf(full_name,"%s/%s",full_name,dirp->d_name);
+			sprintf(current_dir_name,"%s",dirp->d_name);
+			
+			if((strcmp(current_dir_name,".") != 0) && (strcmp(current_dir_name, "..") != 0)){ // it is a normal directory
+				// printf("Current dir name: '%s' - %d\n", current_dir_name, strcmp(current_dir_name,".."));
+				explore_object(full_name);
+			}
+		}	
+		return;
 	}
 }
 
 int main(int argc, char* argv[]){
 	
 	char dir_1[MAXC], dir_2[MAXC];
-	
+	char current_dir_name[MAXC];
 	strcpy(dir_1, argv[1]);
 	strcpy(dir_2, argv[2]);
 	
@@ -70,9 +76,11 @@ int main(int argc, char* argv[]){
 	
 	while((dirp = readdir(dp)) != NULL){
 		sprintf(full_name,"%s/%s",dir_1,dirp->d_name);
-		printf("Examinating the path %s", full_name);
-		
-		explore_directory(full_name);
+		sprintf(current_dir_name, "%s", dirp->d_name);
+		if((strcmp(current_dir_name,".") != 0) && (strcmp(current_dir_name, "..") != 0)){ // it is a normal directory or a file
+			// printf("Current dir name: '%s' - %d\n", current_dir_name, strcmp(current_dir_name,".."));
+			explore_object(full_name); // this function can be called either on a file or on a directory
+		}
 	}
 	return 0;
 }
