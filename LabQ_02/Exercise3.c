280bf215e1a408fa6d425486cafb2592d8de73ea
mattyred <mattiarosso99@gmail.com>
2022-03-17 22:39:44 +0100

    finish the Ex3 but not prints size

--- LabQ_02/Exercise3.c
+++ LabQ_02/Exercise3.c
@@ -10,77 +10,95 @@
 
 #define MAXC 31
 
-void explore_object(char* full_name){
+void visit(DIR* dp1, DIR* dp2, char* path_dir1, char* path_dir2){
+	struct dirent *dirp1, *dirp2;
+	char path_dir1_obj[MAXC], obj[MAXC], path_dir2_obj[MAXC];
 	struct stat buf;
-	struct dirent *dirp;
-	char current_dir_name[MAXC];
-	DIR* dp;
-	
-	// Get information about the current element
-	lstat(full_name, &buf);
+	int fd;
 
-	// Check if it is a file or a directory
-	if(S_ISREG(buf.st_mode) != 0){ // it is a file, return
-		printf("%s : It is a file\n",full_name);
-		return;
-	}else if(S_ISDIR(buf.st_mode) != 0){ // it is a directory, recur
-		printf("%s : It is a directory\n", full_name);
-
-		// Open the directory
-		if((dp = opendir(full_name)) == NULL){
-			printf("Error in opening %s\n", full_name);
-			exit(1);
-		}
-			
-		// visit each element and if it is a directory visit it recursively
-		while((dirp = readdir(dp)) != NULL){
-			sprintf(full_name,"%s/%s",full_name,dirp->d_name);
-			sprintf(current_dir_name,"%s",dirp->d_name);
-			
-			if((strcmp(current_dir_name,".") != 0) && (strcmp(current_dir_name, "..") != 0)){ // it is a normal directory
-				// printf("Current dir name: '%s' - %d\n", current_dir_name, strcmp(current_dir_name,".."));
-				explore_object(full_name);
-			}
-		}	
-		return;
+	// open the directory dir_1
+	if((dp1 = opendir(path_dir1)) == NULL){
+		printf("Error in opening %s\n", path_dir1);
+		exit(1);
 	}
+
+			
+	// visit each element
+	while((dirp1 = readdir(dp1)) != NULL){
+		sprintf(obj,"%s",dirp1->d_name); // current object inside dp1
+		
+		if((strcmp(obj,".") != 0) && (strcmp(obj, "..") != 0)){ // if the current object is . or .. skip
+			// build the full name: path_dir1/object for visiting dir_1
+			sprintf(path_dir1_obj,"%s/%s",path_dir1,dirp1->d_name);
+			
+			// build the full name for dir_2
+			sprintf(path_dir2_obj,"%s/%s",path_dir2,dirp1->d_name);
+			
+			// obtain information about path_dir1/object in dir_1
+			lstat(path_dir1_obj, &buf);
+			
+			if(S_ISDIR(buf.st_mode) != 0){ // path_dir1/object is a directory			
+				printf("Current name D: '%s'\n", obj);
+				
+				// create the directory inside the path of dir_2
+				mkdir(path_dir2_obj, 0777); // create the dir with full privileges
+				
+				// visit the directory 'path_dir1/object' recursively
+				visit(dp1, dp2, path_dir1_obj, path_dir2_obj);
+				
+			}else{ // it is a file, continue iterating but inside path_dir1/
+				printf("Current name F: '%s'\n", obj);
+				
+				// create the same file inside dir_2
+				fd = open(path_dir2_obj, O_RDWR | O_CREAT | O_TRUNC, 0777);
+				
+				// appen two lines at the beginning
+				off_t size_of_file = buf.st_size;
+
+				write(fd, obj, sizeof(obj));
+				//write(fd, " ", sizeof(char *));
+				write(fd, &size_of_file, sizeof(off_t));
+				
+				strcpy(path_dir1, path_dir1_obj); 
+				strcpy(path_dir2, path_dir2_obj); 
+			}
+		}else{ // directory . or ..
+			continue;	
+		}
+	}
+	
+	return;
 }
+
 
 int main(int argc, char* argv[]){
 	
 	char dir_1[MAXC], dir_2[MAXC];
-	char current_dir_name[MAXC];
 	strcpy(dir_1, argv[1]);
 	strcpy(dir_2, argv[2]);
 	
-	// Copy the tree rooted in dir_1 inside the tree rooted in dir_2
-	// For each file copied it has to be put on top:
-	// name of the file ; size of the file
-	
 	struct stat buf;
-	DIR* dp;
-	char full_name[100];
-	struct dirent *dirp;
+	DIR *dp1,*dp2;
 	
-	lstat(dir_1, &buf); // get information about the first folder
+	lstat(dir_1, &buf); // get information about dir_1
 	if(S_ISDIR(buf.st_mode) == 0){
 		printf("Error, %s is not a directory!", argv[1]);
 		exit(1);
 	}
-	if((dp = opendir(dir_1)) == NULL){
-		printf("Error in opening %s", argv[1]);
+	
+	// open dir_1
+	if((dp1 = opendir(dir_1)) == NULL){
+		printf("Error in opening(main) %s", argv[1]);
 		exit(1);
 	}
 	
-	int i = 0;
-	
-	while((dirp = readdir(dp)) != NULL){
-		sprintf(full_name,"%s/%s",dir_1,dirp->d_name);
-		sprintf(current_dir_name, "%s", dirp->d_name);
-		if((strcmp(current_dir_name,".") != 0) && (strcmp(current_dir_name, "..") != 0)){ // it is a normal directory or a file
-			// printf("Current dir name: '%s' - %d\n", current_dir_name, strcmp(current_dir_name,".."));
-			explore_object(full_name); // this function can be called either on a file or on a directory
-		}
+	// open dir_2
+	if((dp2 = opendir(dir_2)) == NULL){
+		printf("Error in opening(main) %s", argv[2]);
+		exit(1);
 	}
+	
+
+	visit(dp1, dp2, dir_1, dir_2);
 	return 0;
 }
